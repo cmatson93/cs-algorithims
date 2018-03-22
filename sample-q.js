37ab641aa3c5dec2227e8a5a18decd955b97fea0
Christina  Matson <cmatson93@gmail.com>
2018-03-22 12:23:06 -0700

    Sample questions

---
+++ sample-q.js
@@ -0,0 +1,30 @@
+//What will this code output to the console?
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
+function foo1() {
+    return {
+        bar: "hello"
+    };
+}
+
+function foo2() {
+    return {
+        bar: "hello"
+    };
+}
+
+console.log(foo1());
+console.log("----");
+console.log(foo2());
