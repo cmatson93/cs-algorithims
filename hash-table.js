c9a4d7e4a220d13e312ba330a5152eae71e64ac1
Christina  Matson <cmatson93@gmail.com>
2018-03-22 11:30:31 -0700

    Updated working hashtable

--- hash-table.js
+++ hash-table.js
@@ -26,6 +26,7 @@ HashTable.prototype.hash = function(key) {
 //when inserting or updating a node we are running the key through our hashing function and then checking for collisions - if there is a collision, we traverse the bucket's chain and find the last node and insert at that postion
 HashTable.prototype.insert = function(key, value) {
     let index = this.hash(key);
+    console.log(index);
     if (!this.buckets[index]) {
         this.buckets[index] = new HashNode(key, value)
     } else if (this.buckets[index].key === key) {
@@ -47,27 +48,43 @@ HashTable.prototype.insert = function(key, value) {
 HashTable.prototype.get = function(key) {
     let index = this.hash(key)
     if (!this.buckets[index]) {
+        console.log("null");
         return null;
     }
     let currentNode = this.buckets[index];
     while (currentNode) {
         if (currentNode.key === key) {
+            console.log(currentNode);
+            console.log(currentNode.value);
             return currentNode.value;
         }
         currentNode = currentNode.next;
     }
+    console.log("null");
     return null;
 }
 
 //iterates through all buckets, and checks for chains in each bucket - returns array of all hash nodes
 HashTable.prototype.retrieveAll = function() {
     let allNodes = [];
-    for (let i = 0; iMthis.numBuckets; i++) {
-        let currentNode = this.buckets[i]
-        while (currentNode) {
-            allNodes.push(currentNode);
-            currentNode = currentNode.next;
-        }
-    }
-    return allNodes;
+    console.log("allNodes", allNodes);
+    console.log(this.numBuckets);
+    console.log(this.buckets);
+    console.log(this.buckets[0]);
+    // for (let i = 0; this.numBuckets; i++) {
+    //     let currentNode = this.buckets[i]
+    //     console.log(this.buckets[i]);
+    //     // while (currentNode) {
+    //     //     allNodes.push(currentNode);
+    //     //     currentNode = currentNode.next;
+    //     // }
+    // }
+    // console.log(allNodes);
+    // return allNodes;
 }
+
+myHT.insert("Christina", 24);
+myHT.insert("Tommy", 25);
+myHT.insert("Riley", 20);
+myHT.get("Christina");
+console.log(myHT[24]);
