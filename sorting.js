37ad2550af3461f02f099ef5a3b5590f0bda2add
Christina  Matson <cmatson93@gmail.com>
2018-03-22 10:44:15 -0700

    Hash Table

--- sorting.js
+++ sorting.js
@@ -6,32 +6,11 @@ animals.sort();
 arr = [10, 7, 6, 5, 3, 9, 4, 8, 1, 2];
 
 function swap(array) {
-    var smallest = 100000;
-    var currentNum;
-    var newNum;
-    var smallestIndex;
-    console.log(array);
-    var counter = 0;
+    var minValue = array[i];
     for (let i = 0; i < array.length; i++) {
-        counter = counter + 1;
-        console.log("counter", counter);
-        console.log("index", i);
-        currentNum = array[i];
-        console.log("currentNum", currentNum);
-        for (let t = 1; t < array.length; t++) {
-            newNum = array[t];
-            console.log("newNum", newNum);
-            if (newNum < currentNum && smallest) {
-                smallest = newNum;
-                console.log("smallest", smallest);
-                smallestIndex = t;
-                console.log("smallestIndex", smallestIndex);
-            }
+        if (array[i] < smallest) {
+            smallestIndex = i;
         }
-        array[i] = smallest;
-        array[smallestIndex] = currentNum;
-        console.log(array);
-
     }
 }
 
